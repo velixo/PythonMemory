a04d8110c4fcd0d9524c216772bf675327aab1d6
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2015-06-24 21:54:40 +0200

    Implemented all methods

--- memory_board.py
+++ memory_board.py
@@ -4,28 +4,86 @@ import random
 class MemoryBoard:
 
 	def __init__(self, rows: int, columns: int):
-		pass
+		self.rows = rows
+		self.columns = columns
+		self.selected_coords = []
+		self.values = self.generate_values(rows, columns)
+		self.grid = self.generate_grid(rows, columns, self.values)
 
 	def get_rows(self) -> int:
-		pass
+		return self.rows
 
 	def get_columns(self) -> int:
-		pass
+		return self.columns
 
 	def select_item(self, i: int, j: int) -> bool:
-		pass
+		if len(self.selected_coords) < 1:
+			self.selected_coords.append((i, j))
+			return False
+		elif len(self.selected_coords == 1):
+			self.selected_coords.append((i, j))
+		selected_vals = self.get_selected_values()
+		self.selected_coords.clear()
+		return selected_vals[0] == selected_vals[1]
 
 	def get_selected_item_coords(self) -> list:
-		pass
+		return self.selected_coords
 
 	def generate_values(self, rows: int, columns: int) -> list:
-		pass
+		if (rows * columns) % 2 != 0:
+			raise ValueError('rows * columns must be an even number')
+		values = []
+		max_val = (int)(rows * columns / 2)
+		for i in range(1, max_val + 1):
+			values.append(i)
+			values.append(i)
+		random.shuffle(values)
+		return values
 
 	def generate_grid(self, rows: int, columns: int, values: list) -> list:
-		pass
+		grid = [None] * columns
+		for i in range(columns):
+			grid[i] = [None] * rows
+
+		for i in range(rows):
+			for j in range(columns):
+				grid[i][j] = values[i * columns + j]
+		return grid
 
 	def print_grid(self, show_values: bool=False):
-		pass
+		grid_str = ''
+		if show_values:
+			spacing = ' ' + ' ' * len(str(max(self.values)))
+			for i in range(self.rows):
+				grid_str += '['
+				for j in range(self.columns):
+					val = self.grid[i][j]
+					adjusted_space = spacing[len(str(val)):]
+					grid_str += adjusted_space + str(val)
+				grid_str += spacing[1:] + ']\n'
+
+		else:
+			selected_vals = self.get_selected_values()
+			# added an extra space as the length of a stringifed
+			# value in the grid is always at least 1
+			spacing = ' ' * len(str(max(selected_vals))) + ' '
+
+			for i in range(self.rows):
+				grid_str += '['
+				for j in range(self.columns):
+					if self.selected_coords.contains((i, j)):
+						val = self.grid[i][j]
+						adjusted_space = spacing[len(str(val)):]
+						grid_str += adjusted_space + str(val)
+					else:
+						grid_str += spacing[1:] + 'x'
+					grid_str += spacing[1:] + ']\n'
+
+		print(grid_str)
 
 	def get_selected_values(self) -> tuple:
-		pass
+		selected_vals = []
+		for coord in self.selected_coords:
+			i, j = *coord
+			selected_vals.append(self.grid[i][j])
+		return (val for val in selected_vals)
