23959b1fd2447c12cb5f72f3366345ed9eb66cd0
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2015-06-27 12:59:28 +0200

    Trying to fix python button grid layout

--- memory_gui.py
+++ memory_gui.py
@@ -11,13 +11,22 @@ class MemoryWindow:
 
 		self.root = tkinter.Tk()
 		self.root.title("Memory")
+		self.content = tkinter.Frame(self.root)
+		self.btn_grid = tkinter.Frame(
+			self.content, borderwidth=5,
+			width=500, height=400
+		)
 
 		for i in range(self.rows):
 			for j in range(self.columns):
 				val = self.memory.get_val_at(i, j)
 				grid_btn = tkinter.Button(
-					self.root, text=str(val),
-					command=lambda: btn_callback(i, j, val))
-				grid_btn.grid(row=i, column=j)
+					self.btn_grid,
+					text=str(val),
+					command=lambda: btn_callback(i, j, val),
+				)
+				grid_btn.grid(row=i, column=j, sticky="NSWE")
 
+		self.content.grid(row=0, column=0)
+		self.btn_grid.grid(row=0, column=0)
 		self.root.mainloop()
