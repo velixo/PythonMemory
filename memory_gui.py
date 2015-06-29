e168a3f52fc4a28148c6a3382a631924ffc5ee27
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2015-06-29 22:45:42 +0200

    Added show values menubar, show_btn_val function

--- memory_gui.py
+++ memory_gui.py
@@ -8,23 +8,54 @@ class MemoryWindow:
 		self.memory = memory
 		self.rows = memory.get_rows()
 		self.columns = memory.get_columns()
+		self.btns = [None] * self.rows
+		for i in range(self.rows):
+			self.btns[i] = [None] * self.columns
 
-		self.root = tkinter.Tk()
-		self.root.title("Memory")
-		self.content = tkinter.Frame(self.root)
+		root = tkinter.Tk()
+		root.title("Memory")
+		content = tkinter.Frame(root)
 
+		show = tkinter.BooleanVar(root)
+		show.set(False)
+		menubar = tkinter.Menu(content)
+		menubar.add_checkbutton(
+			label="Show values",
+			variable=show,
+			command=lambda: self.show_values(show.get()))
+
+		content = tkinter.Frame(root)
 		for i in range(self.rows):
 			for j in range(self.columns):
-				val = self.memory.get_val_at(i, j)
+				val = memory.get_val_at(i, j)
 				grid_btn = tkinter.Button(
-					# self.btn_grid,
-					self.content,
+					content,
 					text=str(val),
 					width=8,
 					height=3,
-					command=lambda i=i, j=j, val=val: btn_callback(i, j, val)
-				)
-				grid_btn.grid(row=i, column=j, sticky="NSWE")
+					command=lambda i=i, j=j, val=val: btn_callback(i, j, val))
 
-		self.content.grid(row=0, column=0)
-		self.root.mainloop()
+				grid_btn.grid(row=i, column=j, sticky="NSWE")
+				self.btns[i][j] = grid_btn
+
+		content.grid(row=0, column=0)
+		root.config(menu=menubar)
+		root.mainloop()
+
+	def show_values(self, show: bool):
+		# print('show_values')
+		# print('checkbtn_checked = ' + str(self.checkbtn_checked.get()))
+		for i in range(self.rows):
+			for j in range(self.columns):
+				if show:
+					val = self.memory.get_val_at(i, j)
+					self.btns[i][j].config(text=str(val))
+				else:
+					self.btns[i][j].config(text='X')
+
+	def show_btn_val(self, i: int, j: int, show: bool):
+		if show:
+			val = self.memory.get_val_at(i, j)
+			self.btns[i][j].config(text=str(val))
+		else:
+			self.btns[i][j].config(text='X')
