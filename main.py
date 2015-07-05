462d1a52ea81aefa7f5fc9c7e6e142b6dfd3a287
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2015-07-05 22:47:49 +0200

    Fixed up main and gui

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-import memory_model
-import memory_gui
+from memory_model import MemoryModel
+from memory_gui import MemoryWindow
 
 rows, columns = (4, 5)
-memory = memory_model.MemoryModel(rows, columns)
+memory = MemoryModel(rows, columns)
 print(memory.grid_str_repr(True))
+score = 0
 
 
-def btn_callback(i: int, j: int, val: int):
+def btn_callback(i: int, j: int, gui: MemoryWindow):
+	global score
+	print(' gui = ' + str(gui) + '\n')  # #####
+	rows = memory.get_rows()
+	columns = memory.get_columns()
+	val = memory.get_val_at(i, j)
 	print(str((i, j)) + " -> " + str(val))
 
-gui = memory_gui.MemoryWindow(memory, btn_callback)
+	match = memory.select_item(i, j)
+	selected_coords = memory.get_selected_coords()
+	matched_coords = memory.get_matched_coords()
+	for n in range(rows):
+		for m in range(columns):
+			if (n, m) in matched_coords:
+				# print(str((n, m)) + ' in matched_coords\n')  # #####
+				gui.set_btn_text(n, m, 'M')
+			elif (n, m) in selected_coords:
+				print(str((n, m)) + ' in ' + str(selected_coords) + ' \n')  # #####
+				coord_val = memory.get_val_at(n, m)
+				gui.set_btn_text(n, m, str(coord_val))
+			else:
+				gui.set_btn_text(n, m, 'X')
+
+	if match:
+		score += 1
+		print('Score is ' + str(score))
+
+
+gui = MemoryWindow(memory, btn_callback)
+gui.start()
--- memory_gui.py
+++ memory_gui.py
@@ -12,11 +12,11 @@ class MemoryWindow:
 		for i in range(self.rows):
 			self.btns[i] = [None] * self.columns
 
-		root = tkinter.Tk()
-		root.title("Memory")
-		content = tkinter.Frame(root)
+		self.root = tkinter.Tk()
+		self.root.title("Memory")
+		content = tkinter.Frame(self.root)
 
-		show = tkinter.BooleanVar(root)
+		show = tkinter.BooleanVar(self.root)
 		show.set(False)
 		menubar = tkinter.Menu(content)
 		menubar.add_checkbutton(
@@ -24,25 +24,27 @@ class MemoryWindow:
 			variable=show,
 			command=lambda: self.show_values(show.get()))
 
-		content = tkinter.Frame(root)
+		content = tkinter.Frame(self.root)
 		for i in range(self.rows):
 			for j in range(self.columns):
-				val = memory.get_val_at(i, j)
 				grid_btn = tkinter.Button(
 					content,
 					text='X',
 					width=8,
 					height=3,
-					command=lambda i=i, j=j, val=val: btn_callback(i, j, val))
+					command=lambda i=i, j=j: btn_callback(i, j, self))
 
 				grid_btn.grid(row=i, column=j, sticky="NSWE")
 				self.btns[i][j] = grid_btn
 
 		content.grid(row=0, column=0)
-		root.config(menu=menubar)
-		root.mainloop()
+		self.root.config(menu=menubar)
+
+	def start(self):
+		self.root.mainloop()
 
 	def show_values(self, show: bool):
+		print('self = ' + str(self) + '\n')  # #####
 		for i in range(self.rows):
 			for j in range(self.columns):
 				if show:
@@ -51,9 +53,6 @@ class MemoryWindow:
 				else:
 					self.btns[i][j].config(text='X')
 
-	def show_btn_val(self, i: int, j: int, show: bool):
-		if show:
-			val = self.memory.get_val_at(i, j)
-			self.btns[i][j].config(text=str(val))
-		else:
-			self.btns[i][j].config(text='X')
+	def set_btn_text(self, i: int, j: int, txt: str):
+		# print(str(i) + str(j) + txt)
+		self.btns[i][j].config(text=txt)
